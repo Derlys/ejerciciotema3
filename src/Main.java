3c0fff70ffde9383b03b1a9c308d50b18b05ac88
Derlys Dominguez <derlys275@gmail.com>
2022-04-19 15:25:30 -0500

    implement: first part exercise

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int result;
+        result = sumar(10,10,30);
+        System.out.println(result);
+    }
+    public static int sumar ( int num1, int num2, int num3){
+        return num1 + num3 + num3;
+
+    }
+}
+
